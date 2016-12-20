8eb4ff119c8bba5c59f2f0c3fdf16d561ac75a7d
Ekerot <daniel.ekerot@gmail.com>
2016-12-21 00:28:10 +0100

    second try

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4567;
 
 app.engine('handlebars', hbs({
     defaultLayout: 'layout',
