1fdd7893e1c2b02bf4bc9cb0eb172d2613632047
Ekerot <daniel.ekerot@gmail.com>
2016-12-30 20:27:31 +0100

    webhooks up and running, config websocket

--- server.js
+++ server.js
@@ -12,20 +12,14 @@ const   path = require('path');
 const   mongoose = require('./config/configDB.js');
 const   websocket = require('./lib/websocket.js');
 
+let GithubWebHook = new require('express-github-webhook');
+let webhookHandler = GithubWebHook({ path: '/webhook', secret: 'secret' });
+
 const   app = express();
 const   port = process.env.PORT || 3000;
 
 mongoose();
 websocket();
-
-app.post('/hooks', function (req, res) {
-
-    console.log(res);
-
-    res.json({
-        message: 'ok got it!'
-    });
-});
 
 app.set('view engine', 'handlebars');app.engine('handlebars', hbs({
     defaultLayout: 'layout',
@@ -41,6 +35,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
 app.use('/', require('./routes/main.js'));
+
+app.use(webhookHandler); // use our middleware
+
+// Now could handle following events
+webhookHandler.on('*', function (event, repo, data) {
+
+});
+
+webhookHandler.on('issues', function (repo, data) {
+
+});
+
+webhookHandler.on('dekes03-examination-3', function (event, data) {
+});
+
+webhookHandler.on('error', function (err, req, res) {
+});
+
 
 //console what port that the app uses
 app.listen(port, () => console.log(`Express app listening on port ${port}!`
