b39bffaffe8dbbac40510578152ba7f2b8e662ed
Ekerot <daniel.ekerot@gmail.com>
2017-01-01 22:39:10 +0100

    webhooks up and running, config websocket

--- routes/main.js
+++ routes/main.js
@@ -51,28 +51,4 @@ router.route('/')  //function just to show first page
 
     });
 
-router.route('/hooks').post(function (req, res){
-    const githubhook = new require('express-github-webhook');
-
-    let github = githubhook({
-        host: "api.github.com",
-        protocol: "https",
-        path: "/hooks",
-        secret: "hoppetisnoppeti"
-    });
-
-        github.on('*', function (event, repo, data) {
-            console.log('hejhopp')
-        });
-
-        github.on('issues', function (repo, data) {
-            console.log('hejhopp')
-        });
-
-        github.on('error', function (err, req, res) {
-            console.log('err')
-        });
-
-});
-
 module.exports = router;
