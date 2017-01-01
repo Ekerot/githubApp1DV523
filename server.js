55f272a5a9071e0e7108523b65a15142a96143ce
Ekerot <daniel.ekerot@gmail.com>
2017-01-01 21:45:07 +0100

    webhooks up and running, config websocket

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ const   session = require('express-session');
 const   hbs = require('express-handlebars');
 const   bodyParser = require('body-parser');
 const   path = require('path');
-const   mongoose = require('./config/configDB.js');
 const   GitHubWebHook = new require('express-github-webhook');
 const   webhookHandler = GitHubWebHook({path: '/webhook', secret: process.env.SECRET_TOKEN});
 
 const   app = express();
 const   port = process.env.PORT || 3000;
-
-mongoose();
 
 // Set Express routes.
 app.get('/', (req, res) => {
