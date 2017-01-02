41a0ae640764db0bd61c655df5d534673f2cf0fb
Ekerot <daniel.ekerot@gmail.com>
2017-01-02 10:11:37 +0100

    webhooks up and running, config websocket

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,11 @@
  * Created by ekerot on 2017-01-01.
  */
 
+var io = require('socket.io');
+
+var socket = io.connect('https://localhost:3000');
+
+socket.on('webhook', function(hook){
+     console.log(hook)
+
+});
