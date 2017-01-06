3c2f39f8ade800337c0f0d7164df8a4afd678521
Ekerot <daniel.ekerot@gmail.com>
2017-01-06 13:46:30 +0100

    changes in dependencies

--- routes/login-github.js
+++ routes/login-github.js
@@ -139,13 +139,14 @@ router.route('/issues/:name')
         github.repos.createHook({
             "name": "webb",
             "active": "true",
-            "secret": "kljfd9823u4nfkls923nfdjks989324",
             "events": [
                 "issues",
                 "issue_comment"
             ],
             "config": {
                 "url": "https://www.ekerot.se/webhook",
+                "secret": "kljfd9823u4nfkls923nfdjks989324",
+                "insecure_ssl": "1",
                 "content_type": "json"
             }
         }, function(err, req, res){
