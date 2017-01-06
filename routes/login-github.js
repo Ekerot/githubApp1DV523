1484dbb8a2d846388b1ab5fb283065577dc2a131
Ekerot <daniel.ekerot@gmail.com>
2017-01-06 14:17:15 +0100

    changes in dependencies

--- routes/login-github.js
+++ routes/login-github.js
@@ -137,6 +137,8 @@ router.route('/issues/:name')
         //TODO: Create new hook does not work, fix it!
 
         github.repos.createHook({
+            "owner": request.user.username,
+            "repo": request.params.name,
             "name": "webb",
             "active": true,
             "secret": "kljfd9823u4nfkls923nfdjks989324",
