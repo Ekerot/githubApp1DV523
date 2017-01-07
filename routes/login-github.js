faee0940f83bd7fb9c266c1cf4df7d95043bb28e
Ekerot <daniel.ekerot@gmail.com>
2017-01-08 00:16:49 +0100

    chenging layout adding OAUTH 3d party authorization

--- routes/login-github.js
+++ routes/login-github.js
@@ -133,17 +133,17 @@ router.route('/:name')
             token: process.env.AUTH_TOKEN
         });
 
-        let username = response.user.username;
 
-        github.repos.pingHook({owner:request.user.username}, {repo:request.params.name},
-            {id:request.session.repo.id}, (req, res) => {
+
+        github.repos.pingHook({owner:request.session.user.username}, {repo:request.params.name},
+            {id:request.session.repo.repo.id}, (req, res) => {
 
                 console.log(req)
 
             } );
 
         github.repos.createHook({
-            "owner": username,
+            "owner": request.session.user.username,
             "repo": request.params.name,
             "name": "web",
             "active": true,
@@ -166,7 +166,7 @@ router.route('/:name')
 //TODO: Get sessionId get repository ID seperate usersSession and use session to store values
 
 //get all issues from selected repo
-        github.issues.getForRepo({owner: request.user.username, repo: request.params.name}, function (err, req) {
+        github.issues.getForRepo({owner: request.session.user.username, repo: request.params.name}, function (err, req) {
 
             let jsonObject = req;
 
