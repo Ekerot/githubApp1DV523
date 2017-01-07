ee9df108c3e757f8abe193ac261f6cf98de7f139
Ekerot <daniel.ekerot@gmail.com>
2017-01-07 12:32:45 +0100

    chenging layout adding OAUTH 3d party authorization

--- routes/login-github.js
+++ routes/login-github.js
@@ -31,7 +31,7 @@ passport.deserializeUser(function(obj, done) {
 
 passport.use(new GitHubStrategy({                           //making a strategy to login with oauth2
         clientID: "7e66ee29510aa0f4db54",
-        clientSecret: "57ee389384a77d0417635e2dfe29273c5f987c72",
+        clientSecret: "2284eb7c2af97ba1151befe9a98a3f009afda80c",
         callbackURL: "https://www.ekerot.se/auth/github/callback"
     },
     function (accessToken, refreshToken, profile, done) {
