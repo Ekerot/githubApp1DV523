820746e05335b3a393b4d05271a8140df6dd5b08
Ekerot <daniel.ekerot@gmail.com>
2017-01-07 20:11:07 +0100

    chenging layout adding OAUTH 3d party authorization

--- routes/login-github.js
+++ routes/login-github.js
@@ -140,9 +140,6 @@ router.route('/:name')
 
             console.log(request.user)
 
-            console.log(request.params.name);
-            console.log(request.user.username)
-
             console.log(request)
 
             //TODO: Get sessionId get repository ID seperate usersSession and use session to store values
