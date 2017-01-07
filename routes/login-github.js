b446f91cf961ddb83573cb201d71e688204cfddd
Ekerot <daniel.ekerot@gmail.com>
2017-01-08 00:38:57 +0100

    chenging layout adding OAUTH 3d party authorization

--- routes/login-github.js
+++ routes/login-github.js
@@ -134,7 +134,7 @@ router.route('/:name')
             token: process.env.AUTH_TOKEN
         });
 
-        github.repos.pingHook({repo:response.params.name},
+        github.repos.pingHook({repo:request.params.name},
             {id:response.repo.id}, (err, req, res) => {
 
                 console.log(req)
