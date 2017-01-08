18781a1f44078436629368790e1a7a2281777dbb
Ekerot <daniel.ekerot@gmail.com>
2017-01-08 01:11:08 +0100

    chenging layout adding OAUTH 3d party authorization

--- routes/login-github.js
+++ routes/login-github.js
@@ -136,7 +136,7 @@ router.route('/:name')
 
         console.log(request.user._json.login)
 
-        github.repos.testHook({repo:request.params.name},{owner:request.user._json.login}, (err, req, res) => {
+        github.repos.testHook({owner:request.user._json.login},{repo:request.params.name}, (err, req, res) => {
 
                 console.log(req)
 
